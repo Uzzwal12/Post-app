436abc68054d57c43b2486c5e669fb1f701cb677
Uzzwal Gupta <uzzwal.gupta@hoolime.co>
2021-04-07 18:13:56 +0530

    basic post page created

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,11 +1,56 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+import { Grid } from "semantic-ui-react";
 
+import PostCard from "../components/postCard";
 const Home = () => {
+  const {
+    loading,
+    data:{ getPosts: posts },
+  } = useQuery(FETCH_POSTS_QUERY);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Post</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          posts &&
+          posts.map((post) => (
+            <Grid.Column key={post.id}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
   );
 };
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      userName
+      createdAt
+      likes {
+        userName
+      }
+      comments {
+        id
+        body
+        userName
+        createdAt
+      }
+      likeCount
+      commentCount
+    }
+  }
+`;
 
 export default Home;
